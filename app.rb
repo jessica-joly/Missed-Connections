07d21aae971bd614aba79c4cae12a3a23325906a
Jennifer Bell & Austin Kincaid <student@epicodus.com>
2015-09-01 15:48:08 -0700

    Altered styling

--- app.rb
+++ app.rb
@@ -15,12 +15,18 @@ get('/users/:id') do
   user_id = params.fetch('id').to_i()
   @user = User.find(user_id)
   @keywords = @user.keywords()
-  @posts = user.posts()
+  @posts = @user.posts()
   erb(:user)
 end
 
 post('/users/new') do
   name = params.fetch('name')
   new_user = User.create({:name => name})
-  redirect('/users/#{new_user.id()}')
+  redirect("/users/#{new_user.id()}")
+end
+
+post('/keywords/new') do
+  keyword = params.fetch('new_keyword')
+  new_keyword = Keyword.create({:keyword => keyword})
+  redirect back
 end
