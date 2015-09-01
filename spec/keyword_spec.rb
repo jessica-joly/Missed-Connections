003327f0328e605ef584ed92f726c30f4aefb3aa
Alyssa Horrocks & Jessica Joly <student@epicodus.com>
2015-09-01 11:14:50 -0700

    All spec pass for class connections

--- spec/keyword_spec.rb
+++ spec/keyword_spec.rb
@@ -1,1 +1,11 @@
 require 'spec_helper'
+
+describe Keyword do
+  describe '#posts' do
+    it { should have_many(:posts) }
+  end
+
+  describe '#users' do
+    it { should have_many(:users).through(:posts) }
+  end
+end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -1,1 +1,18 @@
 require 'spec_helper'
+
+describe Post do
+  describe '#user' do
+    it { should belong_to(:user) }
+  end
+
+  describe '#keyword' do
+    it { should belong_to(:keyword) }
+  end
+
+  describe '#url' do
+    it 'return the URL of a given post' do
+      test_post = Post.create({:url => 'url', :keyword_id => nil, :user_id => nil})
+      expect(test_post.url()).to(eq('url'))
+    end
+  end
+end
