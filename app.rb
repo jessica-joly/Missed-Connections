8715fa261fddd474839720d209d7f1dc63bdc0ea
Jennifer Bell & Jessica Joly <student@epicodus.com>
2015-09-03 11:03:13 -0700

    Add collect user email addres to user page

--- app.rb
+++ app.rb
@@ -108,3 +108,11 @@ patch('/users/:id/update') do
   user.update({:name => name})
   redirect back
 end
+
+patch('/users/:id/email') do
+  user_id = params.fetch('id').to_i()
+  user = User.find(user_id)
+  email = params.fetch('email')
+  user.update({:email => email})
+  redirect back
+end
