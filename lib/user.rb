5af70f3ae8de8d1495a1098cd82fff3cb32690b5
Jennifer Bell & Jessica Joly <student@epicodus.com>
2015-09-03 14:19:22 -0700

    Add validation for email uniqueness

--- lib/user.rb
+++ lib/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :keywords, through: :keywords_users
   has_many :keywords
+  validates_uniqueness_of :email
 
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -8,5 +8,12 @@ describe User do
   describe '#keywords' do
     it { should have_many(:keywords).through(:posts) }
   end
-  
+
+  describe 'validation' do
+    it 'validates uniqueness of user email' do
+      test_user1 = User.create({:name => "Austin", :email => "austin@gmail.com", :password => "cheese"})
+      test_user2 = User.new({:name => "Fred", :email => "austin@gmail.com", :password => "nugget"})
+      expect(test_user2.save()).to(eq(false))
+    end
+  end
 end
